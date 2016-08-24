71567cb27f9f83129c904cb27b87e086a0d342a3
ruiwang <ruiwang@ruiwangdeimac.local>
2016-08-24 14:58:24 +0800

    Initial Commit

---
+++ sizeClass/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  sizeClass
+//
+//  Created by ruiwang on 16/8/24.
+//  Copyright © 2016年 ruiwang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ sizeClass/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  sizeClass
+//
+//  Created by ruiwang on 16/8/24.
+//  Copyright © 2016年 ruiwang. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
