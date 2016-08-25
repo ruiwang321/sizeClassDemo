cadc88bc8ff017a0a750c64fb06c25d0ab089479
ruiwang <ruiwang@ruiwangdeiMac.local>
2016-08-25 09:46:43 +0800

    init

--- sizeClass/ViewController.m
+++ sizeClass/ViewController.m
@@ -7,21 +7,54 @@
 //
 
 #import "ViewController.h"
+#import <objc/message.h>
 
-@interface ViewController ()
+@interface ViewController () {
+    CALayer *_layer;
+}
+
+@property (weak, nonatomic) IBOutlet UIView *contentView;
+
 
 @end
 
 @implementation ViewController
+- (IBAction)back:(id)sender {
+    if ([[UIDevice currentDevice] respondsToSelector:@selector(setOrientation:)]) {
+
+        objc_msgSend([UIDevice currentDevice], @selector(setOrientation:), UIInterfaceOrientationPortrait);
+    }
+}
+- (IBAction)fullScreen:(id)sender {
+    if ([[UIDevice currentDevice] respondsToSelector:@selector(setOrientation:)]) {
+
+        objc_msgSend([UIDevice currentDevice], @selector(setOrientation:), UIInterfaceOrientationLandscapeLeft);
+    }
+    
+}
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    
+    _layer = [[CALayer alloc] init];
+    _layer.backgroundColor = [[UIColor redColor] CGColor];
+    [self.contentView.layer addSublayer:_layer];
+}
+
+- (void)viewDidLayoutSubviews {
+    [super viewDidLayoutSubviews];
+    
+    [self.contentView setNeedsLayout];
+    [self.contentView layoutIfNeeded];
+    
+    _layer.frame = CGRectMake(0, 0, self.contentView.frame.size.width, self.contentView.frame.size.height);
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+
 
 @end
